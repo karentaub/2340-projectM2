59c198953700855eedb2b9399070410ff86a9b7d
Devany Sandoval <sandovad96@gmail.com>
2017-01-31 23:10:55 -0500

    added Person5 method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,10 +30,15 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
-	}
-	
+        String answer = "";
+        if(input ==  null) {
+            return answer;
+        }
+        String temp = input.substring(0,2);
+        answer += input.substring(2) + temp;
+        return answer;
+    }
+    
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
